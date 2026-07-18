a4d9580809a61c7697bd7a720b37131bd3e9232a
jjis936 <evanjoyner2012@gmail.com>
2026-07-17 20:25:00 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+# Discord
+TOKEN=your_discord_bot_token
+CLIENT_ID=your_discord_application_id
+GUILD_ID=your_server_id_here
+# ^ leave GUILD_ID blank/remove it to deploy commands globally instead
+#   of to one server. Useful if guild-specific deploys keep failing.
+
+VOUCH_CHANNEL_ID=1507682259678003371
+LEAVE_VOUCH_CHANNEL_ID=1524746787636772964
+BRAND_NAME=Sinner Services
+
+# SMS number rental providers
+FIVESIM_API_KEY=your_5sim_api_key
+SMSPOOL_API_KEY=your_smspool_api_key
